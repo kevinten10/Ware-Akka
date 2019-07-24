efa5187376a03db6450a674db5eaacd067a3ce20
Kevin_T <596823919@qq.com>
2019-07-24 21:49:35 +0800

    Init : init akka-main

---
+++ akka-sample-main-scala/src/main/scala/sample/hello/Greeter.scala
@@ -0,0 +1,19 @@
+package sample.hello
+
+import akka.actor.Actor
+
+object Greeter {
+
+  case object Greet
+
+  case object Done
+
+}
+
+class Greeter extends Actor {
+  def receive = {
+    case Greeter.Greet =>
+      println("Hello World!")
+      sender() ! Greeter.Done
+  }
+}
